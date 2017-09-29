e328cc9f41189d49dc0408f6c0492e42c1775b4b
USP <USP@DESKTOP-TNO3FK2>
2017-09-29 15:05:02 -0500

    Numer mayor y menor

---
+++ numeros.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Numeros{
+
+    function MayorDosNumeros($n1,$n2){
+        if($n1>$n2)
+            return $n1;
+        else
+            return $n2;
+    }
+
+    function MenorDosNumeros($n1,$n2){
+        if($n1<$n2)
+            return $n1;
+        else
+            return $n2;
+    }
+}
